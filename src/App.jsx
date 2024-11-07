38d31df872f7a97dde014549e473c1326053bbc2
tejudharanesh <tejudharanesh1234@gmail.com>
2024-11-07 13:09:07 +0530

    added required folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import Hero from "./sections/Hero/Hero";
 function App() {
+  const [product, setProduct] = useState(false);
+  const handleProduct = () => {
+    setProduct(!product);
+  };
   return (
     <div className="w-full px-4 xl:max-w-screen-xl xl:mx-auto flex justify-center">
       <Hero />
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import DarkBackground from "../../assets/desktopImages/DarkBackground.png";
 import building from "../../assets/desktopImages/Building.png";
 import river from "../../assets/videos/RiverflowVideo.mp4";
+import habiHomes from "../../assets/videos/habiHomes.mov";
 function Hero() {
   return (
     <div
-      className="h-[515px] md:h-[1027px] w-full bg-cover bg-center bg-no-repeat relative"
+      className="min-h-[300px] h-[515px] md:h-[1027px] w-full bg-cover bg-center bg-no-repeat relative"
       style={{
         backgroundImage: `url(${DarkBackground})`,
       }}
     >
-      <div className="absolute bottom-0 object-cover h-[897px]">
+      <div className="absolute bottom-0 object-cover md:h-[897px] w-full">
         <video autoPlay loop muted playsInline>
-          <source src={river} type="video/mp4" />
+          <source src={habiHomes} type="video/mp4" />
         </video>
       </div>
-      <img src={building} alt="" className="absolute top-12 left-10" />
     </div>
   );
 }
