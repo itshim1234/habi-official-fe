4303a12a3abcf66734d235a2d83451be6ff6e03b
tejudharanesh <tejudharanesh1234@gmail.com>
2024-11-09 17:09:23 +0530

    added styles

--- src/sections/Info/Info.jsx
+++ src/sections/Info/Info.jsx
@@ -25,7 +25,7 @@ function Info() {
 
   return (
     <div className="flex justify-center bg-[#111111]">
-      <div className="container min-w-full h-[70vh] md:h-[80vh] lg:h-[90vh] xl:h-[80vh]">
+      <div className="container min-w-full h-[55vh] md:h-[80vh] lg:h-[90vh] xl:h-[80vh]">
         <div>
           <h1 className="text-[48px] text-white font-larken-bold mt-10 md:mb-10">
             Why habi
@@ -34,10 +34,10 @@ function Info() {
         <div className="hero-circle">
           <div className="inner-text top-[30%] md:top-[21%] lg:top-[20%] xl:top-[17%]">
             <div>
-              <p className="font-giloryM text-[24px] md:text-[32px]">
+              <p className="font-giloryM text-[24px] md:text-[32px] mb-[8px]">
                 {texts[currentIndex]}
               </p>
-              <p className="font-giloryM text-[16px] md:text-[18px]">
+              <p className="font-giloryM text-[16px] md:text-[18px] px-4">
                 {desc[currentIndex]}
               </p>
             </div>
--- src/sections/Service/Service.jsx
+++ src/sections/Service/Service.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import model from "../../assets/videos/model.mov";
+import arrow from "../../assets/images/ArrowRight.png";
 
 function Service() {
   return (
-    <div className="flex flex-col justify-center text-center bg-black text-white">
+    <div className="flex flex-col justify-center text-center bg-black text-white relative">
       <hr className="bg-[#f8f8ff] p-0 m-0" />
-      <h1 className="font-larken-bold text-[32px] md:text-[40px] lg:text-[48px]">
+
+      <button className="flex w-fit mt-[57px] md:mt-0 md:absolute md:top-[100px] md:left backdrop-blur-[12.5px] bg-slate-50 bg-opacity-30 p-5 rounded-r-xl border border-gray-400 border-l-0">
+        <span className="mr-3">BaaP</span> <img src={arrow} alt="" />
+      </button>
+
+      <h1 className="font-larken-bold text-[32px] md:text-[40px] lg:text-[48px] mt-[22px] md:mt-[56px]">
         habi homes
       </h1>
-      <p className="text-[24px] md:text-[32px] lg:text-[40px] font-giloryB">
+      <p className="text-[24px] md:text-[32px] lg:text-[40px] font-giloryB mt-[80px]">
         One-Stop Design Centric
       </p>
       <p className="text-[18px] md:text-[24px] lg:text-[32px] font-giloryS">
         Construction Company
       </p>
-      <p className="text-[16px] lg:text-[18px] font-giloryM w-[309px] md:w-[424px] mx-auto">
+      <p className="text-[16px] lg:text-[18px] font-giloryM w-[309px] md:w-[424px] mx-auto mt-4">
         Tailored personalized living spaces that are custom-designed to meet the
         unique needs and preferences of each client. Every home reflects the
         client’s individual taste, lifestyle, and standard of living, creating a
