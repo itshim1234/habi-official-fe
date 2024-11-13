0062e86b8874b51b1b78a87e2c8df27a3abf1c99
tejudharanesh <tejudharanesh1234@gmail.com>
2024-11-13 15:19:14 +0530

    added responsive

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,11 +8,11 @@ import "./hero.css";
 
 function Hero() {
   return (
-    <div className="relative min-h-[300px] h-screen w-screen bg-cover bg-center">
+    <div className="relative min-h-[300px] h-[460px] md:h-[750px] lg:h-screen w-screen bg-cover bg-center">
       {/* Video background */}
       <div className="absolute inset-0 z-0 bg-cover">
         <video
-          className="object-cover w-screen h-screen"
+          className="object-cover w-screen h-[450px] md:h-[750px] lg:h-screen"
           autoPlay
           loop
           muted
@@ -50,7 +50,7 @@ function Hero() {
         ></div>
 
         {/* Main text */}
-        <div className="relative text-[48px] md:text-[96px] lg:text-[114px] font-larken-bold leading-[50px] md:leading-[100px] lg:leading-[120px]">
+        <div className="relative text-[48px] md:text-[85px] lg:text-[80px] 2xl:text-[114px] font-larken-bold leading-[50px] md:leading-[80px] lg:leading-[100px] 2xl:leading-[120px]">
           {/* Blurred background above the text */}
           <div
             className="absolute -top-3 left-0 right-0 mx-auto z-40"
@@ -70,7 +70,7 @@ function Hero() {
         </div>
       </div>
       <div className="absolute z-20 text-white justify-center text-center right-[16px] md:right-[32px] lg:right-[82px] top-[20px] md:top-[40px]">
-        <button className="flex items-center justify-between px-5 w-[140px] md:w-[180px] h-[56px] md:h-[60px] rounded-lg border-2 border-white/20 bg-transparent backdrop-blur-md text-white font-semibold">
+        <button className="flex items-center justify-between px-5 w-[140px] md:w-[180px] h-[56px] md:h-[60px] rounded-lg border-2 border-white/20 bg-black/25 backdrop-blur-md text-white font-semibold">
           <span className="text-[18px] md:text-[24px] mb-1 font-giloryS">
             Login
           </span>
@@ -82,16 +82,16 @@ function Hero() {
           />
         </button>
       </div>
-      <div className="relative z-20 text-white justify-center text-center top-[50%] flex space-x-4 md:space-x-12 text-md md:text-[24px]">
-        <button className="px-5 w-[167] md:w-[275px] h-12 md:h-[75px] rounded-xl border-2 border-white/20 bg-black/30 backdrop-blur text-white flex items-center justify-center font-giloryS">
+      <div className="relative z-20 text-white justify-center text-center top-[50%] flex space-x-4 md:space-x-12 text-md md:text-[20px] 2xl:text-[24px]">
+        <button className="px-5 w-[160] md:w-[230px] 2xl:w-[275px] h-12 md:h-[60px] lg:h-[60px] 2xl:h-[80px] rounded-xl border-2 border-white/20 bg-black/30 backdrop-blur text-white flex items-center justify-center font-giloryS">
           Explore Projects
         </button>
 
-        <button className="px-5 py-2 w-[167] md:w-[275px] h-12 md:h-[75px] rounded-xl bg-primary text-white flex items-center justify-center font-giloryS">
-          Free Consultation
+        <button className="px-5 w-[160] md:w-[230px] 2xl:w-[275px] h-12 md:h-[60px] lg:h-[60px] 2xl:h-[80px] rounded-xl bg-primary text-white flex items-center justify-center font-giloryS">
+          Free Consultatio6
         </button>
       </div>
-      <div className="relative z-20 text-white justify-center text-center top-[55%] hidden lg:flex space-x-4 md:space-x-12 text-md md:text-[24px] ">
+      <div className="relative z-20 text-white justify-center text-center top-[55%] hidden 2xl:flex space-x-4 md:space-x-12 text-md md:text-[24px] ">
         <img src={scroll} alt="" />
       </div>
     </div>
