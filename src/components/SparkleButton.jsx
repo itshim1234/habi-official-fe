012993cbef9fc71e811e77312bce74f247e598ab
Himanshu Vaishya <himanshu@habi.one>
2025-08-05 23:16:34 +0530

    bapp button fixed

--- src/components/SparkleButton.jsx
+++ src/components/SparkleButton.jsx
@@ -16,103 +16,127 @@ const rotate = keyframes`
 
 const StyledWrapper = styled.div`
   .button {
-    --black-700: hsla(0, 0%, 30%, 1);
-    --transition: 0.3s ease-in-out;
+    --black-700: hsla(0 0% 30% / 1);
+    --border_radius: 18px;
+    --transtion: 0.3s ease-in-out;
+    --offset: 2px;
 
     cursor: pointer;
     position: relative;
-    border-radius: ${({ isBaaP }) =>
-      isBaaP ? "0 24px 24px 0" : "24px 0 0 24px"};
+    border-radius: ${({ text }) =>
+      text == "BaaP" ? "0 24px 24px 0" : "24px 0 0 24px"};
     display: flex;
     align-items: center;
     gap: 0.5rem;
-    padding: 24px 2rem;
-    transform-origin: center;
-    transform: scale(calc(1 + (var(--active, 0) * 0.1)));
-    transition: transform var(--transition);
-  }
 
-  .button::before,
-  .button::after,
-  .dots_border {
-    border-radius: ${({ isBaaP }) =>
-      isBaaP ? "0 24px 24px 0" : "24px 0 0 24px"};
+    transform-origin: center;
+
+    padding: 24px 2rem;
+
+    transform: scale(calc(1 + (var(--active, 0) * 0.1)));
+
+    transition: transform var(--transtion);
   }
 
   .button::before {
     content: "";
-    background-color: var(--black-700);
+    border-radius: ${({ text }) =>
+      text == "BaaP" ? "0 24px 24px 0" : "24px 0 0 24px"};
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+
     width: 100%;
     height: 100%;
+    background-color: var(--black-700);
+
+    transition: all var(--transtion);
     z-index: 0;
-    transition: all var(--transition);
   }
 
   .button::after {
     content: "";
-    background: linear-gradient(to top, #00eaff 0px, transparent 100%);
-    background-color: #046f7a;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+    border-radius: ${({ text }) =>
+      text === "BaaP" ? "0 24px 24px 0" : "24px 0 0 24px"};
     width: 100%;
     height: 100%;
+    background-color: #046f7a;
+
+    background-image: linear-gradient(to top, #00eaff 0px, transparent 100%);
+    background-position: top;
+
     opacity: var(--active, 0);
+    transition: opacity var(--transtion);
     z-index: 2;
-    transition: opacity var(--transition);
   }
 
   .button:is(:hover, :focus-visible) {
     --active: 1;
   }
-
   .button:active {
     transform: scale(1);
   }
 
-  .dots_border {
+  .button .dots_border {
     --size_border: calc(100% + 2px);
+
+    overflow: hidden;
+    border-radius: ${({ text }) =>
+      text === "BaaP" ? "0 24px 24px 0" : "24px 0 0 24px"};
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+
     width: var(--size_border);
     height: var(--size_border);
-    overflow: hidden;
     background-color: transparent;
+
     z-index: -10;
   }
 
   .button .dots_border::before {
     content: "";
     position: absolute;
-    top: 50%;
+    top: 30%;
     left: 50%;
-    width: 100%;
-    height: 100%;
     transform: translate(-50%, -50%);
-    border-radius: inherit;
-    
-    background: conic-gradient(
-      from 0deg,
-      hsla(0, 0%, 100%, 0.5),
-      transparent 30%,
-      hsla(0, 0%, 100%, 0.5) 60%,
-      transparent 100%
-    );
-    animation: ${rotate} 3s linear infinite;
-    z-index: -1;
+    transform-origin: left;
+    transform: rotate(0deg);
+
+    width: 100%;
+    height: 2rem;
+    background-color: white;
+
+    mask: linear-gradient(transparent 0%, white 120%);
+    animation: rotate1 2s linear infinite;
   }
 
-  .text_button {
-    color: white;
+  @keyframes rotate1 {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .button .sparkle {
     position: relative;
     z-index: 10;
+
+    width: 1.75rem;
+  }
+
+  .button .text_button {
+    position: relative;
+    z-index: 10;
+
+    background-clip: text;
+
+    color: white;
   }
 `;
 
@@ -213,20 +237,26 @@ const SparkleButton = ({
     );
   }
   return (
-    <StyledWrapper isBaaP={text === "BaaP"}>
-      <button className="button font-gilroyS">
+  <StyledWrapper text={text}>
+      <button className="button font-giloryS">
         <div className="dots_border" />
         {text === "BaaP" ? (
           <>
             <span className="text_button text-2xl 2xl:text-3xl">{text}</span>
-            <img src={arrow} alt="arrow" className="z-10" />
+            <img
+              src={arrow}
+              alt="Home construction Bengaluru"
+              className={`z-10`}
+            />
           </>
         ) : (
           <>
-            <img src={arrow} alt="arrow" className="z-10 rotate-180" />
-            <span className="text_button text-2xl 2xl:text-3xl text-center leading-tight whitespace-pre-line">
-              {text}
-            </span>
+            <img
+              src={arrow}
+              alt="Home construction Bengaluru"
+              className={`z-10 rotate-180`}
+            />
+            <span className="text_button text-2xl 2xl:text-3xl">{text}</span>
           </>
         )}
       </button>
